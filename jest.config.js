f7580f461557b18b057b5c6daa75fb514360e3c7
Gary Chambers <gary@repro.dev>
2026-09-19 23:01:28 +0100

    test: ignore nested worktrees in Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: 'jest-expo',
-  testPathIgnorePatterns: ['/node_modules/', '/.superpowers/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.superpowers/', '/.worktrees/'],
+  modulePathIgnorePatterns: ['/node_modules/', '/.worktrees/'],
 }
